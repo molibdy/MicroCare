03ae439a56409827de649ab36ae313e6ffc3dc0e
molibdy <molipotts@gmail.com>
2021-04-06 21:26:27 +0200

    Subida calendario (vista de dia)

--- microcareApp/src/app/app.module.ts
+++ microcareApp/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     MaterialModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
     FormsModule
   ],
   providers: [],
